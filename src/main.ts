3017800e73ad25fc377f430fa3896e8eeb9f7a98
dartsia <nichisniukd.7b@gmail.com>
2026-01-04 15:33:22 +0200

    cors fix

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   app.use(cookieParser());
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://teacher-report-frontend.vercel.app'],
     credentials: true,
   });
 
